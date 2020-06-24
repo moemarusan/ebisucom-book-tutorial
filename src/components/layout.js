baa7ff63dcb38ff1cddb59ef1a3932afb878042b
moemarusan <moemarusan@gmail.com>
2020-06-24 23:33:32 +0900

    :sparkles:[update] Chapter4まで

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,9 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 
 import "./layout.css"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import {config} from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
 export default ({children}) => (
     <div>
---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1 style={{padding: "20vh 0", textAlign: "center"}}>お探しのページは見つかりませんでした</h1>
+    </Layout>
+)
